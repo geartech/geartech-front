47ea8d7263a1d7d28a24fc719ef2f83722e6a41c
leandro-mbs <leandromarques_21@hotmail.com>
2025-05-24 14:54:16 -0300

    Impl. Inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import "antd/dist/reset.css";
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Ant Design!</h1>
+      <Button type="primary">Botão Antd</Button>
+    </div>
+  );
+}
