c8dc9a6c006d04e06cffa0c5ac4f8981f1481632
leandro-mbs <leandromarques_21@hotmail.com>
2025-05-31 18:49:10 -0300

    impl 01

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,11 @@
 'use client';
 
 import '@/app/styles/globals.css';
-import MasterLayout from '@/components/layout/MasterLayout';
-import ThemeRegistry from '@/components/theme/ThemeRegistry';
-import { ThemeModeProvider } from '@/components/theme/ThemeContext';
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR">
-      <body>
-        <ThemeModeProvider>
-          <ThemeRegistry>
-            <MasterLayout>{children}</MasterLayout>
-          </ThemeRegistry>
-        </ThemeModeProvider>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 'use client';
 
 export default function Home() {
-  return <div>TESTE</div>;
+  return <div>LOGIN</div>;
 }
