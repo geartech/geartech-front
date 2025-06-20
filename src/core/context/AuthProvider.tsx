01b9333280206a02c56ddc950ebbb5110cea1ecf
leandro-mbs <leandromarques_21@hotmail.com>
2025-06-20 15:56:31 -0300

    lauth

--- src/core/context/AuthProvider.tsx
+++ src/core/context/AuthProvider.tsx
@@ -1,47 +1,36 @@
 'use client';
 
 import { createContext, useContext, useEffect, useState } from 'react';
-import { getToken, logout } from '../auth';
 import { UserLoginDTO } from '../sdk/api';
 import { geartechApi } from '../sdk';
 
 interface AuthContextType {
   user: UserLoginDTO | undefined;
-  loading: boolean;
   logout: () => void;
 }
 
 const AuthContext = createContext<AuthContextType>({
   user: undefined,
-  loading: true,
   logout: () => {},
 });
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<UserLoginDTO | undefined>({});
-  const [loading, setLoading] = useState(true);
 
   const fetchUser = async () => {
     try {
       const res = await geartechApi.auth.getLoggedUser();
       setUser(res.data);
     } catch {
-      logout();
+      // logout();
     } finally {
       setLoading(false);
     }
   };
 
-  useEffect(() => {
-    const token = getToken();
-    if (token) {
-      fetchUser();
-    } else {
-      setLoading(false);
-    }
-  }, []);
+  useEffect(() => {}, []);
 
-  return <AuthContext.Provider value={{ user, loading, logout }}>{children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{ user, logout }}>{children}</AuthContext.Provider>;
 };
 
 export const useAuth = () => useContext(AuthContext);
